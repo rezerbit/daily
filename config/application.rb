e44489364b43ddda7ed68c85cf1ba8f79ff620fd
rezerbit <rezerbit@gmail.com>
2018-11-13 16:21:24 +0500

    Remove sprockets

--- config/application.rb
+++ config/application.rb
@@ -25,15 +25,16 @@ Dotenv.load(
 
 module Daily
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.view_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
